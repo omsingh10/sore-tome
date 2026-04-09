be06e74675cc5a8947f13c41679413e93f7948e6
Kruturaj Padwal <dkruturaj@gmail.com>
2026-04-10 02:26:13 +0530

    chore: update auto-generated plugin files and lockfile

--- sero/macos/Flutter/GeneratedPluginRegistrant.swift
+++ sero/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,17 +6,27 @@ import FlutterMacOS
 import Foundation
 
 import cloud_firestore
+import file_picker
+import file_selector_macos
 import firebase_auth
 import firebase_core
 import firebase_messaging
+import flutter_local_notifications
 import path_provider_foundation
+import printing
 import shared_preferences_foundation
+import sqflite_darwin
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
+  FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
+  FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
+  FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  PrintingPlugin.register(with: registry.registrar(forPlugin: "PrintingPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
 }
